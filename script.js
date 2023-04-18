5180184fb8214493dc040aa16486e7f7d837c6f0
AmritK000 <87861815+AmritK000@users.noreply.github.com>
2023-04-18 23:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
 const team = ["Jhn", "Bb", "Alce", "Pop"];
 const team1 = ["Joh", "Bo", "Ali", "Pop"];
-const cap1= ["Joh", "Bo", "Ali", "Pop"];
 
 
 const person = {
+  name: "John Doe",
+  age: 80,
+};
+const cap1 = {
   name: "John Doe",
   age: 80,
 };
